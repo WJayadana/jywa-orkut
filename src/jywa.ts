31d29d78350578e019b62e9155023863d0cdc73a
WJayadana <wjayadana@gmail.com>
2025-09-05 19:03:28 +0700

    Initial commit jywa-orkut package

---
+++ src/jywa.ts
@@ -0,0 +1,11 @@
+// Main export
+import JywaOrkut from "./JywaOrkut.js";
+
+// Type exports for better IDE support
+export * from "./types.js";
+
+// Named export
+export { JywaOrkut };
+
+// Default export
+export default JywaOrkut;
